169e97eea35b638eddfc34f111fc7a465ae8bce7
Anirban Haldar <65188009+Anirban600@users.noreply.github.com>
2022-08-02 23:58:34 +0530

    Testing download feature with real file links

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var materials = [
   ["Week 1"],
-  ["02/08/2022", "Material 1", "#"],
-  ["03/08/2022", "Material 2", "#"],
-  ["04/08/2022", "Material 3", "#"],
+  ["02/08/2022", "Material 1", "https://github.com/IITBHILAI-NLP/DS503_CoursePage/raw/gh-pages/Atharva.png"],
+  ["03/08/2022", "Material 2", "https://github.com/IITBHILAI-NLP/DS503_CoursePage/raw/gh-pages/Atharva.png"],
+  ["04/08/2022", "Material 3", "https://github.com/IITBHILAI-NLP/DS503_CoursePage/raw/gh-pages/Atharva.png"],
 ]
 
 var announcements = [
